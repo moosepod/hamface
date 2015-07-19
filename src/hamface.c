7d75e35475521d37a13343b56c01da8184539a95
Your Name <you@example.com>
2015-07-18 20:34:48 -0400

    Added date to zulu time area

--- src/hamface.c
+++ src/hamface.c
@@ -1,56 +1,48 @@
 #include <pebble.h>
+
+/* Main file for hamface Pebble watch face.
   
-#define KEY_TEMPERATURE 0
-#define KEY_CONDITIONS 1
+   Displays local and utc time/date.
+
+   by Matt Christensen (mchristensen@moosepod.com)
+*/  
+
   
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_utctime_layer;
 
+// Update the local time layer from the pebble's time
 static void update_time() {
-  // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
   
-  // Create a long-lived buffer
   static char buffer[] = "00:00";
 
-  // Write the current hours and minutes into the buffer
   if(clock_is_24h_style() == true) {
-    //Use 2h hour format
     strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
   } else {
-    //Use 12 hour format
     strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
   }
 
-  // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, buffer);
 }
 
+// Update the UTC time layer from the pebble's time
 static void update_utc_time() {
-  // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = gmtime(&temp);
   
-  // Create a long-lived buffer
-  static char buffer[] = "00:00";
+  static char buffer[] = "00:00 | JAN 00"; 
+  
+  strftime(buffer, sizeof("00:00 | JAN 00"), "%H:%M | %b %d", tick_time);
 
-  // Write the current hours and minutes into the buffer
-  if(clock_is_24h_style() == true) {
-    //Use 2h hour format
-    strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
-  } else {
-    //Use 12 hour format
-    strftime(buffer, sizeof("00:00"), "%I:%M", tick_time);
-  }
-
-  // Display this time on the TextLayer
   text_layer_set_text(s_utctime_layer, buffer);
 }  
 
+// Called when the main window is initialized
 static void main_window_load(Window *window) {
-  // Create time TextLayer
+  // Setup the main time layer
   Layer *window_layer = window_get_root_layer(window);
   GRect window_bounds = layer_get_bounds(window_layer);
   s_time_layer = text_layer_create(GRect(0,5, window_bounds.size.w,100));
@@ -60,23 +52,24 @@ static void main_window_load(Window *window) {
   text_layer_set_text_alignment(s_time_layer,GTextAlignmentCenter);
   text_layer_set_text(s_time_layer, "00:00");
 
-  // Create UTC time layer
+  // Setup the UTC time layer
   s_utctime_layer = text_layer_create(GRect(0,window_bounds.size.h-50,window_bounds.size.w,window_bounds.size.h));
-  text_layer_set_font(s_utctime_layer, fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK));
+  text_layer_set_font(s_utctime_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_background_color(s_utctime_layer, GColorClear);
   text_layer_set_text_color(s_utctime_layer, GColorBlack);
   text_layer_set_text_alignment(s_utctime_layer,GTextAlignmentCenter);
-  text_layer_set_text(s_utctime_layer, "00:00");
+  text_layer_set_text(s_utctime_layer, "00:00 JAN 01");
 
-
-  // Add it as a child layer to the Window's root layer
+  // Add both layers to main window
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
   layer_add_child(window_layer, text_layer_get_layer(s_utctime_layer));
   
+  // Initial refresh of times.
   update_time();
   update_utc_time();
 }
 
+// Called when the window is destroyed.
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
   text_layer_destroy(s_time_layer);
@@ -84,30 +77,30 @@ static void main_window_unload(Window *window) {
 
 }
 
+// Update any information that needs updating when time changes
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
   update_utc_time();
 }
 
 static void init() {
-  // Create main Window element and assign to pointer
+  // Create the main window and store pointer
   s_main_window = window_create();
 
-  // Set handlers to manage the elements inside the Window
+  // Connect the load and unload handlers to the window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
     .load = main_window_load,
     .unload = main_window_unload
   });
 
-  // Show the Window on the watch, with animated=true
+  // Display the main window on the watch, true will animate its arrival
   window_stack_push(s_main_window, true);
   
-  // Register with TickTimerService
+  // Register so our tick handler is called every minute
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 }
 
 static void deinit() {
-  // Destroy Window
   window_destroy(s_main_window);
 }
 
